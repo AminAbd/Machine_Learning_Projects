2a4d00c5278d690b116e157ce87402d657b5e1f6
AminAbd <ameen.abdelmutalab@gmail.com>
2024-02-14 12:38:32 -0500

    insurance commit

--- Health Insurance Cost/Simple_Regression_insurance.py
+++ Health Insurance Cost/Simple_Regression_insurance.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
@@ -8,10 +9,10 @@ import matplotlib.pylab as plt
 Data=pd.read_csv('Insurance/insurance.csv')
 print(Data.head())
 # Get a concise summary of the Data
-#print(Data.info())
+print(Data.info())
 # Check for missing values
-#print(Data.isnull().sum())
-"""
+print(Data.isnull().sum())
+
 # For binary categorical variables, replace categories with 0 and 1
 Data['sex'] = Data['sex'].map({'female': 0, 'male': 1})
 Data['smoker'] = Data['smoker'].map({'no': 0, 'yes': 1})
@@ -26,37 +27,16 @@ Y = Data['charges']  # Replace 'charges' with the name of your target variable
 
 ##########################
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
-#######
-column_name = X_train.columns[6]  # where 'i' is the column index
-sns.histplot(X_train[column_name], kde=True)
-plt.title(f'Distribution of {column_name}')
-plt.show()
-####
-import numpy as np
-
-
-for i in range(X_train.shape[1]):  # Loop over the number of features (columns)
-    print(i)
-    column_name = X_train.columns[i]  # where 'i' is the column index
-    sns.histplot(X_train[column_name], kde=True)
-    plt.title(f'Distribution of {column_name}')
-    plt.show()
-
-#############################
-
+# Create and train the linear regression model
 model=LinearRegression()
 model.fit(X_train,Y_train)
-
 y_pred = model.predict(X_test)
-
 mse = mean_squared_error( Y_test,y_pred)
 print('mse',mse)
 
-
-
+#############################
 plt.figure()
 plt.scatter(Y_test,y_pred)
 plt.plot([Y_test.min(), Y_test.max()], [Y_test.min(), Y_test.max()], 'k--', lw=2)  # Diagonal line
 plt.show()
 
-"""
